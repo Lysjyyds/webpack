13e87025830bfc455a181d82f424f264922c9cd2
zjh <zjiahe0124@163.com>
2022-06-17 20:32:23 +0800

    完成自动生成index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const { join } = require('path');
-
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry:'./src/main.js',//入口
     output:{
         path:join(__dirname, 'lib') ,//出口路径
         filename:"index.js"
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: 'index.html'
+        })
+      ]
 }
